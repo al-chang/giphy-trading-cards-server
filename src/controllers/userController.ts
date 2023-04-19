f45ed423b98c9e602aed83e8058af253843c082a
al-chang <aac6692@gmail.com>
2023-04-19 17:51:18 -0400

    Better signup logic

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -28,22 +28,6 @@ const getUsers = async (req: Request, res: Response) => {
   // Filters
   const { username, email, role } = req.query;
 
-  const whereClause = {
-    username: {
-      contains: username as string,
-    },
-    email: {
-      contains: email as string,
-    },
-    ...(!!role
-      ? {
-          role: {
-            equals: role as Role,
-          },
-        }
-      : {}),
-  };
-
   const [users, total] = await prisma.$transaction([
     prisma.user.findMany({
       select: fields,
